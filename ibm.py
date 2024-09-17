b4d8cced7fa6347aef90b94d0a628b999a0caf26
Alexander Kibarov <117196657+KibarovCmd@users.noreply.github.com>
2024-09-17 19:54:05 +0300

    Create ibm.py

---
+++ ibm.py
@@ -0,0 +1,17 @@
+import math
+
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+
+points = [(1, 2), (3, 4), (6, 8), (2, 1), (7, 5)]
+
+distances = []
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+
+print(f"En kısa mesafe: {min_distance}")
